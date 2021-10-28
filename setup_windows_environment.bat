c59390f546d4d1e51c0d4a9e816179166ecdd4f7
David Ackerman <ackermand@janelia.hhmi.org>
2021-10-28 17:40:07 -0400

    update edm path so that it works during same prompting

--- setup_windows_environment.bat
+++ setup_windows_environment.bat
@@ -7,10 +7,10 @@ echo "run enthought distribution manager installer"
 %userprofile%\Downloads\edm_2.0.0_x86_64.msi >%logfile% 2>&1
 
 echo "make a new env using the python packaging that comes with amira"
-edm envs import --force -f "C:\Program Files\Thermo Scientific Amira-Avizo3D 2021.1\python\bundles\3dSoftware_win64.json" hxEnv >>%logfile% 2>&1
+%userprofile%\AppData\Local\Programs\Enthought\edm\edm.bat envs import --force -f "C:\Program Files\Thermo Scientific Amira-Avizo3D 2021.1\python\bundles\3dSoftware_win64.json" hxEnv >>%logfile% 2>&1
 
 echo "install python packages"
-edm run -e hxEnv pip install zarr numcodecs olefile et_xmlfile dask[array] >>%logfile% 2>&1
+%userprofile%\AppData\Local\Programs\Enthought\edm\edm.bat run -e hxEnv pip install zarr numcodecs olefile et_xmlfile dask[array] >>%logfile% 2>&1
 
 echo "tell amira to use the python we just installed"
 setx HX_FORCE_PYTHON_PATH %userprofile%\.edm\envs\hxEnv >>%logfile% 2>&1
